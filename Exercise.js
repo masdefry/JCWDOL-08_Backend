5b09494a0941d49fc39e78c382a643e720e271f6
masdefry <muhammaddefryan@gmail.com>
2023-01-09 19:33:42 +0700

    Day-05 Done

--- Exercise.js
+++ Exercise.js
@@ -1,5 +1,11 @@
-Buatlah REST API untuk menghandle request dari users seperti di bawah ini:
-1. User dapat mencari data penumpang berdasarkan nomor tiket
-2. User dapat mencari data penumpang berdasarkan umur dan gender 
-3. User dapat mencari data penumpang berdasarkan nama dan umur
-   ex. response { name: 'Braund, Mr. Owen Harris', sex: 'male', survived: 'pass away/life' }
+// Buatlah REST API untuk menghandle request dari users seperti di bawah ini:
+// 1. User dapat mencari data penumpang berdasarkan nomor tiket
+// 2. User dapat mencari data penumpang berdasarkan umur dan gender 
+// 3. User dapat mencari data penumpang berdasarkan nama dan umur
+//    ex. response { name: 'Braund, Mr. Owen Harris', sex: 'male', survived: 'pass away/life' }
+
+
+
+// HOLIDAY EXERCISE
+// Buat REST API untuk aplikasi to do list, dimana users dapat melakukan
+// get all todo, create todo, update todo dan delete todo
