3de2c53e7195164f01229ab5f97335bd1fb8f20e
masdefry <muhammaddefryan@gmail.com>
2023-01-18 19:09:06 +0700

    Day-12 Setup

--- Exercise.js
+++
@@ -1,3 +0,0 @@
-// 1. Fitur login
-// 2. User dapat melakukan update data address
-// 3. User dapat mengubah password miliknya
--- myorm-api/routers/usersRouter.js
+++ myorm-api/routers/usersRouter.js
@@ -5,6 +5,5 @@ const Router = express.Router()
 const {usersController} = require('../controllers') // Akan otomatis mengambil file index.js nya
 
 Router.post('/register', usersController.register)
-Router.get('/login', usersController.login)
 
 module.exports = Router
