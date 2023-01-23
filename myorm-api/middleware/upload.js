00ff6138ca459e192d64ce69264ca5aacca04341
masdefry <muhammaddefryan@gmail.com>
2023-01-23 21:16:43 +0700

    Day-13 File Size Validation

--- myorm-api/middleware/upload.js
+++ myorm-api/middleware/upload.js
@@ -2,13 +2,27 @@
 const {multerUpload} = require('./../lib/multer')
 
 // Import DeleteFiles
-// const deleteFiles = require('./../helpers/deleteFiles')
+const deleteFiles = require('./../helpers/deleteFiles')
 
 const uploadImages = (req, res, next) => {
     const multerResult = multerUpload.fields([{name: 'images', maxCount: 3}])
     multerResult(req, res, function (err){
         try { 
             if(err) throw err
+
+            req.files.images.forEach(value => {
+                if(value.size > 100000){
+                    deleteFiles(req.files.images)
+
+                    return res.status(404).send({
+                        isError: true, 
+                        message: `${value.originalname} size too large`,
+                        data: null
+                    })
+                }
+            })
+
+            // next()
             
         } catch (error) {
             res.status(400).send({
